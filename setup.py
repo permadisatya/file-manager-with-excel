cf55030645be8a0b3dd01908d2da885b5b6fcc08
permadisatya <satya.permadi.d@gmail.com>
2022-11-12 23:56:34 +0700

    update for package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="FMXL",
+    version="0.1.0",
+    description="Manage file name and attribute in Spreadsheet.",
+    url="https://github.com/permadisatya/file-manager-with-excel",
+    author="Permadi Satya",
+    author_email="satya.permadi.d@gmail.com",
+    license="",
+    packages=["FMXL"],
+)
